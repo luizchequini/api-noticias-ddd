19d2f38b1453486d004e41785cb5b41121bc23b6
Luiz Chequini <46603262+luizchequini@users.noreply.github.com>
2021-10-04 08:21:01 -0300

    Criação das Interfaces dentro do Domínio juntamente com a classe de Serviço de Notícia

---
+++ api-noticias-ddd/Dominio/Servicos/ServicoNoticia.cs
@@ -0,0 +1,55 @@
+﻿using Dominio.Interfaces;
+using Dominio.Interfaces.InterfaceServicos;
+using Entidades.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Servicos
+{
+    public class ServicoNoticia : IServicoNoticia
+    {
+        private readonly INoticia _iNoticia;
+
+        public ServicoNoticia(INoticia iNoticia)
+        {
+            _iNoticia = iNoticia;
+        }
+        public async Task AdicionaNoticia(Noticia noticia)
+        {
+            var validarTitulo = noticia.ValidarPropriedadeString(noticia.Titulo, "Titulo");
+            var validarInformacao = noticia.ValidarPropriedadeString(noticia.Informacao, "Informacao");
+
+            if(validarTitulo && validarInformacao)
+            {
+                noticia.DataCadastro = DateTime.Now;
+                noticia.DataAlteracao = DateTime.Now;
+                noticia.Ativo = true;
+
+                await _iNoticia.Adicionar(noticia);
+            }
+        }
+
+        public async Task AtualizaNoticia(Noticia noticia)
+        {
+            var validarTitulo = noticia.ValidarPropriedadeString(noticia.Titulo, "Titulo");
+            var validarInformacao = noticia.ValidarPropriedadeString(noticia.Informacao, "Informacao");
+
+            if (validarTitulo && validarInformacao)
+            {
+                //noticia.DataCadastro = DateTime.Now;
+                noticia.DataAlteracao = DateTime.Now;
+                noticia.Ativo = true;
+
+                await _iNoticia.Atualizar(noticia);
+            }
+        }
+
+        public async Task<List<Noticia>> ListarNoticiasAtivas()
+        {
+            return await _iNoticia.ListarNoticias(n => n.Ativo);
+        }
+    }
+}
